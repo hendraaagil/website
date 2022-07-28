05a084608f9beb078be32f2d2aa26c4d34ed243f
hendraaagil <hendraaagil@gmail.com>
2022-07-28 20:51:43 +0700

    chore: update giscus & add redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,43 @@
 module.exports = {
   images: { domains: ['images.ctfassets.net'] },
+  async redirects() {
+    // https://twitter.com/LiamHammett/status/1260984553570570240
+    return [
+      {
+        source: '/.env',
+        destination: 'https://www.youtube.com/watch?v=V4MF2s6MLxY',
+        permanent: false,
+      },
+      {
+        source: '/wp-login.php',
+        destination: 'https://www.youtube.com/watch?v=V4MF2s6MLxY',
+        permanent: false,
+      },
+      {
+        source: '/wp-admin',
+        destination: 'https://www.youtube.com/watch?v=V4MF2s6MLxY',
+        permanent: false,
+      },
+      {
+        source: '/me',
+        destination: '/about',
+        permanent: false,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/hendraaagil',
+        permanent: false,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/hendraaagil',
+        permanent: false,
+      },
+      {
+        source: '/source',
+        destination: 'https://github.com/hendraaagil/website',
+        permanent: false,
+      },
+    ];
+  },
 };
