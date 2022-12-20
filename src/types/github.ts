54d49eb955699f3e2b778d505eda18efe419a6cc
hendraaagil <hendraaagil@gmail.com>
2022-12-20 14:52:49 +0700

    feat: add github api

---
+++ src/types/github.ts
@@ -0,0 +1,17 @@
+export type GithubUserProfile = {
+  login: string
+  id: number
+  avatar_url: string
+  name: string
+  public_repos: number
+  public_gists: number
+  followers: number
+  following: number
+}
+
+export type GithubRepository = {
+  id: number
+  name: string
+  fork: boolean
+  stargazers_count: number
+}
