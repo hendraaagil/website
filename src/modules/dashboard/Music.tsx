633f1ad1a57b02f19fa4388cc7334cd138433903
hendraaagil <hendraaagil@gmail.com>
2022-12-26 09:11:11 +0700

    feat: add track skeleton

--- src/modules/dashboard/Music.tsx
+++ src/modules/dashboard/Music.tsx
@@ -1,13 +1,11 @@
 import { Heading } from '@/components'
 import { CurrentlyPlaying, TopTracks } from './spotify'
 
-export const Music = () => {
-  return (
-    <section>
-      <Heading variant="h2">Music</Heading>
-      <CurrentlyPlaying />
-      <Heading variant="h3">Top Tracks</Heading>
-      <TopTracks />
-    </section>
-  )
-}
+export const Music = () => (
+  <section>
+    <Heading variant="h2">Music</Heading>
+    <CurrentlyPlaying />
+    <Heading variant="h3">Top Tracks</Heading>
+    <TopTracks />
+  </section>
+)
--- src/modules/dashboard/spotify/TopTracks.tsx
+++ src/modules/dashboard/spotify/TopTracks.tsx
@@ -4,13 +4,23 @@ import Image from 'next/image'
 import useSWR from 'swr'
 import fetcher from '@/libs/fetcher'
 import { Fragment } from 'react'
-import { FiRotateCw } from 'react-icons/fi'
 import { Hr, Link } from '@/components'
 
 type TrackProps = {
   track: SpotifyTopTrack
   index: number
 }
+
+const TrackSkeleton = () => (
+  <div className="flex animate-pulse items-center space-x-4 rounded px-0 py-2 xs:px-2">
+    <p className="opacity-0">01</p>
+    <div className="h-12 w-12 bg-gray-200 transition-[background-color] duration-300 dark:bg-gray-700"></div>
+    <div className="space-y-2">
+      <div className="h-4 w-48 bg-gray-200 transition-[background-color] duration-300 dark:bg-gray-700"></div>
+      <div className="h-2 w-32 bg-gray-200 transition-[background-color] duration-300 dark:bg-gray-700"></div>
+    </div>
+  </div>
+)
 
 const Track = ({ track, index }: TrackProps) => (
   <div className="flex items-center space-x-4 rounded px-0 py-2 xs:px-2">
@@ -28,16 +38,19 @@ export const TopTracks = () => {
 
   return (
     <div className="space-y-2">
-      {isLoading ? (
-        <FiRotateCw className="animate-spin text-xl" />
-      ) : (
-        data?.tracks.map((track, index) => (
-          <Fragment key={track.id}>
-            <Track track={track} index={index} />
-            {index + 1 !== data.tracks.length && <Hr />}
-          </Fragment>
-        ))
-      )}
+      {!isLoading
+        ? Array.from(Array(10).keys()).map((item) => (
+            <Fragment key={item}>
+              <TrackSkeleton />
+              {item !== 9 && <Hr />}
+            </Fragment>
+          ))
+        : data?.tracks.map((track, index) => (
+            <Fragment key={track.id}>
+              <Track track={track} index={index} />
+              {index + 1 !== data.tracks.length && <Hr />}
+            </Fragment>
+          ))}
     </div>
   )
 }
