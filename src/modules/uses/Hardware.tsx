93426aaae1548f862417580f9efac7de6f41eac3
hendraaagil <hendraaagil@gmail.com>
2023-01-12 19:00:32 +0700

    feat(uses): add link wrapper

--- src/modules/uses/Hardware.tsx
+++ src/modules/uses/Hardware.tsx
@@ -1,5 +1,15 @@
+import React from 'react'
 import uses from '@/_data/uses.json'
-import { Heading, UnorderedList } from '@/components'
+import { Heading, Link, UnorderedList } from '@/components'
+
+const LinkWrapper = ({ children, link }: { children: React.ReactNode; link?: string }) =>
+  link ? (
+    <Link url={link} isExternal>
+      {children}
+    </Link>
+  ) : (
+    <>{children}</>
+  )
 
 export const Hardware = () => {
   const { heading, lists } = uses.hardware
@@ -12,18 +22,20 @@ export const Hardware = () => {
           <Heading variant="h3">{list.heading}</Heading>
           <UnorderedList className="space-y-1">
             {list.lists.map((item) => (
-              <li key={item.name || item.title}>
-                {item.title && <strong>{item.title}</strong>} {item.name}
-                {item.specs && (
-                  <UnorderedList className="space-y-1">
-                    {item.specs?.map((spec) => (
-                      <li key={spec.title}>
-                        <strong>{spec.title}</strong> {spec.name}
-                      </li>
-                    ))}
-                  </UnorderedList>
-                )}
-              </li>
+              <LinkWrapper key={item.name || item.title} link={item.link}>
+                <li>
+                  {item.title && <strong>{item.title}</strong>} {item.name}
+                  {item.specs && (
+                    <UnorderedList className="space-y-1">
+                      {item.specs?.map((spec) => (
+                        <li key={spec.title}>
+                          <strong>{spec.title}</strong> {spec.name}
+                        </li>
+                      ))}
+                    </UnorderedList>
+                  )}
+                </li>
+              </LinkWrapper>
             ))}
           </UnorderedList>
         </div>
