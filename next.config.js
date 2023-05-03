6c0e98c703afa882f6a7d2ca357b81f060ef1e64
hendraaagil <hendraaagil@gmail.com>
2023-05-03 14:39:06 +0700

    chore: configure `next-pwa`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -57,4 +61,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
