ba54f8f4f456123c895e54c515656927ec5decc1
hendraaagil <hendraaagil@gmail.com>
2023-12-15 20:21:26 +0700

    feat: improve accessibility and seo

--- src/components/project/card.tsx
+++ src/components/project/card.tsx
@@ -2,33 +2,33 @@ import type { Project } from 'contentlayer/generated'
 
 import Link from 'next/link'
 import { ChevronRight } from 'lucide-react'
-import { Heading, ImageBlur } from '@/components/ui'
+import { Heading, ImageBlur, headingVariants } from '@/components/ui'
 
 export const ProjectCard = ({ project }: { project: Project }) => (
-  <Link
-    href={`/projects/${project.slug}`}
-    className="block"
-    aria-label="Link project"
-  >
-    <article className="group h-full border transition-colors border-color hover:bg-color-secondary">
-      <div className="relative flex flex-col justify-center overflow-hidden">
-        <ImageBlur
-          blurDataURL={project.thumbnailPlaceholder}
-          src={project.thumbnail}
-          alt={`Thumbnail for project ${project.title}`}
-          width={1200}
-          height={630}
-          className="object-cover group-hover:scale-105"
-        />
-        <div className="absolute flex h-full w-full items-center justify-center space-x-2 opacity-0 transition-opacity group-hover:bg-slate-50/75 group-hover:opacity-100 dark:group-hover:bg-slate-900/75">
-          <span className="text-color">View project</span>
-          <ChevronRight size={16} />
-        </div>
-      </div>
-      <div className="justify-center space-y-1 px-3 py-2">
-        <Heading variant="h3">{project.title}</Heading>
-        <p className="text-color-secondary">{project.description}</p>
-      </div>
-    </article>
-  </Link>
+  <article className="group h-full border transition-colors border-color hover:bg-color-secondary">
+    <div className="relative flex flex-col justify-center overflow-hidden">
+      <ImageBlur
+        blurDataURL={project.thumbnailPlaceholder}
+        src={project.thumbnail}
+        alt={`Thumbnail for project ${project.title}`}
+        width={1200}
+        height={630}
+        className="object-cover group-hover:scale-105"
+      />
+      <Link
+        href={`/projects/${project.slug}`}
+        aria-label={`View details of project: ${project.title}`}
+        className="absolute flex h-full w-full items-center justify-center space-x-2 opacity-0 transition-opacity group-hover:bg-slate-50/75 group-hover:opacity-100 dark:group-hover:bg-slate-900/75"
+      >
+        <span className="text-color">View project</span>
+        <ChevronRight size={16} />
+      </Link>
+    </div>
+    <div className="justify-center space-y-1 px-3 py-2">
+      <Heading variant="h2" className={headingVariants({ variant: 'h3' })}>
+        {project.title}
+      </Heading>
+      <p className="text-color-secondary">{project.description}</p>
+    </div>
+  </article>
 )
