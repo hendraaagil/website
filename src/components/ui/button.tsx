c4c7cddc0f3f5dae7b7d5a4cbc40a0058161fde2
hendraaagil <hendra@dicoding.com>
2024-01-30 14:32:26 +0700

    style: adjust button class

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,35 +4,32 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-slate-50 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-900 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-900 dark:focus-visible:ring-slate-300',
-  {
-    variants: {
-      variant: {
-        default:
-          'bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90',
-        destructive:
-          'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
-        outline:
-          'border bg-color border-color hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
-        secondary:
-          'bg-slate-100 text-color hover:bg-slate-100/80 dark:bg-slate-800 dark:hover:bg-slate-800/80',
-        ghost: 'hover:bg-slate-100 hover:text-color dark:hover:bg-slate-800',
-        link: 'underline-offset-4 text-color hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 px-3',
-        lg: 'h-11 px-8',
-        icon: 'h-10 w-10',
-      },
+const buttonVariants = cva('btn', {
+  variants: {
+    variant: {
+      default:
+        'bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90',
+      destructive:
+        'bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90',
+      outline:
+        'border bg-color border-color hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
+      secondary:
+        'bg-slate-100 text-color hover:bg-slate-100/80 dark:bg-slate-800 dark:hover:bg-slate-800/80',
+      ghost: 'hover:bg-slate-100 hover:text-color dark:hover:bg-slate-800',
+      link: 'underline-offset-4 text-color hover:underline',
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
+    size: {
+      default: 'h-10 px-4 py-2',
+      sm: 'h-9 px-3',
+      lg: 'h-11 px-8',
+      icon: 'h-10 w-10',
     },
   },
-)
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+})
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
